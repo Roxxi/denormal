f9991da3d364ab7832bada09781899243785ddaa
Alex Bahouth <ajbahouth@gmail.com>
2014-02-03 10:11:36 -0800

    Update project.clj
    Up-rev'd to 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
-
 (defproject roxxi/denormal "0.2.4"
   :description "Denormalizes JSON into single rows of data."
   :url "http://www.github.com/roxxi/denormal"
@@ -6,4 +5,4 @@
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [roxxi/clojure-common-utils "0.0.18"]
-                 [com.onekingslane.danger/jsonschema "1.0.0"]])
+                 [com.onekingslane.danger/jsonschema "1.0.1"]])
