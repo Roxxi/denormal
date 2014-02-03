26c102ef29f48ebd19911b8334dfca30bed5cf49
Alex Bahouth <abahouth@onekingslane.com>
2014-02-03 10:16:14 -0800

    Updated versions of clojure and common-utils

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject roxxi/denormal "0.2.4"
+(defproject com.onekingslane.danger/denormal "1.0.0"
   :description "Denormalizes JSON into single rows of data."
   :url "http://www.github.com/roxxi/denormal"
   :license {:name "GNU General Public License Version 3"
             :url "http://www.gnu.org/licenses/gpl-3.0.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [roxxi/clojure-common-utils "0.0.18"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.onekingslane.danger/clojure-common-utils "0.0.19"]
                  [com.onekingslane.danger/jsonschema "1.0.1"]])
